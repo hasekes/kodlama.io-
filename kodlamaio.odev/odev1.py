8f87f7b83aad839eedad00c1d94ec8bd26277954
hasekes <79418762+hasekes@users.noreply.github.com>
2023-03-11 15:07:25 +0300

    kodlama io ödev

---
+++ kodlamaio.odev/odev1.py
@@ -0,0 +1,19 @@
+#1 
+#Veri Tipleri =
+   #String : Metin  veri tipi.
+   #Integer : Sayisal veri tipi.
+   #Float : Ondalikli sayi tipi.
+   #Boolean : True ve false değeri tipi.
+   #List : Listeyi tutar.
+   #dictionry : Sözlük  tutar.
+#2
+#string= başlıklar bir stringtir kurslarım vs.
+#integer= kursun içeriğine girdiğinde kursun yüzde kaç ilerlediğini göstermesi vs.
+#3
+giris = True
+
+if giris==False:
+    print("Kullanıcı girişi başarılıdır")
+else:
+    print("Kullanıcı girişi başarısızdır")
+
